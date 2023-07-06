78a5fadb5a3c5b27f88363bdf9396cd051426fa8
Kovtunenkovv <kovtunenkovv@mail.ru>
2023-07-06 22:51:41 +0300

    Create 2. send_in_time_bot.py

---
+++ 2. send_in_time_bot.py
@@ -0,0 +1,21 @@
+import telebot
+import time
+import datetime
+from datetime import datetime
+
+count = 1
+now = datetime.now().strftime("%H:%M")
+token = '***'                               #toket your bot
+bot = telebot.TeleBot(token)
+chat_id = '***'                             #id your user
+text = 'Good morning!'
+
+while count>=0:
+    now = datetime.now().strftime("%H:%M")
+    if now == '09:00':
+        bot.send_message(chat_id, text) 
+        print(now, 'Done')
+        time.sleep(59)
+    else:
+        print(now, 'None')
+        time.sleep(59)
