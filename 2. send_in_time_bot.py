29b53df206a4e8710900d90e0ea32581cd12ea4a
Kovtunenkovv <92364725+Kovtunenkovv@users.noreply.github.com>
2025-08-07 10:01:30 +0300

    Update 2. send_in_time_bot.py

--- 2. send_in_time_bot.py
+++ 2. send_in_time_bot.py
@@ -19,3 +19,27 @@ while count>=0:
     else:
         print(now, 'None')
         time.sleep(59)
+
+#________________________________________________________
+
+import telebot
+import time
+import datetime
+from datetime import datetime
+
+count = 1
+now = datetime.now().strftime("%H:%M")
+token = '***'                               #toket your bot
+bot = telebot.TeleBot(token)
+chat_id = '***'                             #id your user
+text = 'What happened?'
+
+while count>=0:
+    now = datetime.now().strftime("%H:%M")
+    if now >= '09:00' and now <= '19:00':
+        bot.send_message(chat_id, text) 
+        print(now, 'Done')
+        time.sleep(59)
+    else:
+        print(now, 'None')
+        time.sleep(900)
